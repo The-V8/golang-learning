2804c5cb28d74915f09bb443e4369b20a354a0c1
Oliver Scheer <oliver.scheer@microsoft.com>
2020-07-13 17:51:26 +0000

    Add test

--- daheinze-oscheer/hello/hello_test.go
+++ daheinze-oscheer/hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, Starlord"
+	got := Hello("World")
+	want := "Hello, World"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
