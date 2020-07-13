2abaa656a8e3da5b31e5d1948c4af83dd3eb525e
Oliver Scheer <oliver.scheer@microsoft.com>
2020-07-13 18:01:58 +0000

    add test to hello world

--- daheinze-oscheer/hello/hello_test.go
+++ daheinze-oscheer/hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("World")
-	want := "Hello, World"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Daniel")
+		want := "Hello, Daniel"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
