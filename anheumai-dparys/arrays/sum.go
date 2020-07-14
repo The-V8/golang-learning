8c160533af04238139e81d8cbc57406bc27d1115
Andreas Heumaier <andreas.heumaier@microsoft.com>
2020-07-14 10:26:48 +0200

    Added array functions

--- anheumai-dparys/arrays/sum.go
+++ anheumai-dparys/arrays/sum.go
@@ -8,3 +8,27 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll exposes Summ of severall Arrays
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllTails should be exported fot test
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+
+	}
+	return sums
+}
