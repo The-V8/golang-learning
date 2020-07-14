7b530874d54658342bc33a58fc5481cbc3f85c0f
Oliver Scheer <oliver.scheer@microsoft.com>
2020-07-14 08:25:22 +0000

    added some documentation to iteration

--- daheinze-oscheer/iteration/repeat.go
+++ daheinze-oscheer/iteration/repeat.go
@@ -1,5 +1,6 @@
 package iteration
 
+// Repeat - Repeats a character multiple times
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
