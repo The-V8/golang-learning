f28484d7f2663997ac436c139f91483a622b3152
Dariusz Parys <dparys@microsoft.com>
2020-07-14 08:08:15 +0200

    Added methods to rectangle and circle

--- anheumai-dparys/structs/structs.go
+++ anheumai-dparys/structs/structs.go
@@ -1,11 +1,36 @@
 package structs
 
-// Perimeter calculates the perimeter of a rectangle
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import (
+	"math"
+)
+
+// Rectangle declares a rectangle (comments are just here to please the linter)
+type Rectangle struct {
+	width  float64
+	height float64
 }
 
 // Area calculates the area of a rectangle
-func Area(width, height float64) float64 {
-	return width * height
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+// Circle declares a circle (what a surprise)
+type Circle struct {
+	Radius float64
+}
+
+// Area calculates the area of a circle
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Perimeter calculates the perimeter of a rectangle
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.width + r.height)
+}
+
+// Area calculates the area of a rectangle
+func Area(r Rectangle) float64 {
+	return r.width * r.height
 }
--- anheumai-dparys/structs/structs_test.go
+++ anheumai-dparys/structs/structs_test.go
@@ -11,13 +11,25 @@ func assert(t *testing.T, got, want float64) {
 }
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 	assert(t, got, want)
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
-	assert(t, got, want)
+
+	t.Run("Calculate area of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.0
+		assert(t, got, want)
+	})
+
+	t.Run("Calculate area of a circle", func(t *testing.T) {
+		cicle := Circle{10.0}
+		got := cicle.Area()
+		want := 314.16
+		assert(t, got, want)
+	})
 }
