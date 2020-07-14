2aab2487cae05b0155e4f965e894d102bc7f0c61
Dariusz Parys <dparys@microsoft.com>
2020-07-14 07:52:16 +0200

    Added area function

--- anheumai-dparys/structs/structs.go
+++ anheumai-dparys/structs/structs.go
@@ -1,6 +1,11 @@
 package structs
 
 // Perimeter calculates the perimeter of a rectangle
-func Perimeter(width float64, height float64) float64 {
-	return 0
+func Perimeter(width, height float64) float64 {
+	return 2 * (width + height)
+}
+
+// Area calculates the area of a rectangle
+func Area(width, height float64) float64 {
+	return width * height
 }
--- anheumai-dparys/structs/structs_test.go
+++ anheumai-dparys/structs/structs_test.go
@@ -1,14 +1,23 @@
 package structs
 
-import(
+import (
 	"testing"
 )
 
-func TestPerimeter(t *testing.T) {
-    got := Perimeter(10.0, 10.0)
-    want := 40.0
+func assert(t *testing.T, got, want float64) {
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
 
-    if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
-    }
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+	assert(t, got, want)
+}
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 10.0)
+	want := 100.0
+	assert(t, got, want)
 }
