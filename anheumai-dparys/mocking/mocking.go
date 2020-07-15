47129e77ce74295503f2f73e257656c33c9d12fc
Dariusz Parys <dparys@microsoft.com>
2020-07-15 16:56:58 +0200

    Chapter Mocking
    Closes #35

---
+++ anheumai-dparys/mocking/mocking.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+	write          = "write"
+	sleep          = "sleep"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(w io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(w, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(w, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
