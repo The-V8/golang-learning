eb3e43252408ac8e4da8aae88bfe10e612610e6e
Oliver Scheer <oliver.scheer@microsoft.com>
2020-07-16 16:06:34 +0200

    added SpySleeper

---
+++ daheinze-oscheer/mocking/countdown.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+// Countdown counting a number down to zero. Awesome, right?
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
