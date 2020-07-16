8c743be2762e29c3303cb54de5f02cb8e48c32e2
Oliver Scheer <oliver.scheer@microsoft.com>
2020-07-16 16:25:14 +0200

    cleanup

--- daheinze-oscheer/mocking/countdown.go
+++ daheinze-oscheer/mocking/countdown.go
@@ -26,34 +26,26 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	fmt.Fprint(out, finalWord)
 }
 
-// func Countdown(out io.Writer, sleeper Sleeper) {
-// 	for i := countdownStart; i > 0; i-- {
-// 		sleeper.Sleep()
-// 	}
-
-// 	for i := countdownStart; i > 0; i-- {
-// 		fmt.Fprintln(out, i)
-// 	}
-
-// 	sleeper.Sleep()
-// 	fmt.Fprint(out, finalWord)
-// }
-
+// Sleeper interface
 type Sleeper interface {
 	Sleep()
 }
 
+// DefaultSleeper struct
 type DefaultSleeper struct{}
 
+// Sleep like a baby for a second
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+// ConfigurableSleeper can be configured with duration and a sleep a custom method
 type ConfigurableSleeper struct {
 	duration time.Duration
 	sleep    func(time.Duration)
 }
 
+// Sleep like a baby for a configured amount if seconds
 func (c *ConfigurableSleeper) Sleep() {
 	c.sleep(c.duration)
 }
