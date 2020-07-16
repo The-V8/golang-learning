0d863c713700d0317fad94feccf21b48285b7c1a
Oliver Scheer <oliver.scheer@microsoft.com>
2020-07-16 16:22:18 +0200

    added configurablesleeper

--- daheinze-oscheer/mocking/countdown.go
+++ daheinze-oscheer/mocking/countdown.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
@@ -26,6 +26,19 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	fmt.Fprint(out, finalWord)
 }
 
+// func Countdown(out io.Writer, sleeper Sleeper) {
+// 	for i := countdownStart; i > 0; i-- {
+// 		sleeper.Sleep()
+// 	}
+
+// 	for i := countdownStart; i > 0; i-- {
+// 		fmt.Fprintln(out, i)
+// 	}
+
+// 	sleeper.Sleep()
+// 	fmt.Fprint(out, finalWord)
+// }
+
 type Sleeper interface {
 	Sleep()
 }
@@ -34,4 +47,13 @@ type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
